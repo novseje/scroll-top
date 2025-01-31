c2213713e5c1a2ff40014f614ea4e0c4130532cf
novseje <novseje@gmail.com>
2025-01-31 17:13:20 +0200

    Replace wpfront-scroll-top plugin to simple code

---
+++ footer.php
@@ -0,0 +1,22 @@
+<div id="wpfront-scroll-top-container"><img src="/blog/wp-content/themes/soledad-child/images/scroll-top.png" alt="" title="" /></div>
+<style>
+    #wpfront-scroll-top-container {display:none;position:fixed;cursor:pointer;z-index:9999;opacity:0;right: 20px;bottom: 20px;transition: opacity 0.2s linear;}
+    #wpfront-scroll-top-container img {width: auto;height: auto;}
+</style>
+<script>
+    const scrollTop = document.getElementById("wpfront-scroll-top-container");
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+    scrollTop.onclick = scrollToTop;
+
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            scrollTop.style.opacity = 0.8;
+            scrollTop.style.display = 'block';
+        } else {
+            scrollTop.style.opacity = 0;
+            setTimeout(() => { if (scrollTop.style.opacity === 0) scrollTop.style.display = 'none';}, 200);
+        }
+    }, true);
+</script>
